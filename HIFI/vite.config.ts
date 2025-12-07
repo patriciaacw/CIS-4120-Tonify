33203d38548f1b547bd5b0013762639920722263
riyaagrawall <riya21@seas.upenn.edu>
2025-12-06 21:55:25 -0500

    Add proxy configuration for API requests

--- HIFI/vite.config.ts
+++ HIFI/vite.config.ts
@@ -62,5 +62,11 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000', // your Node server
+        changeOrigin: true,
+      },
+    },
   },
 });
