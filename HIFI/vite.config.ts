745f1ef913ddee7b70525725bcef9216dce1c422
riyaagrawall <riya21@seas.upenn.edu>
2025-12-06 22:24:23 -0500

    Update vite.config.ts

--- HIFI/vite.config.ts
+++ HIFI/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
@@ -60,13 +60,9 @@ export default defineConfig({
     outDir: 'build',
   },
   server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true,
-      },
-    },
+    host: true, // allow external access
+    allowedHosts: [
+      'stridulous-subpericranial-theola.ngrok-free.dev',
+    ],
   },
 });
